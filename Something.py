4078005f71fe775ab0c96286d52cf5a16c5853df
Konstantinos Knais <konstantinosknais@gmail.com>
2020-03-27 19:29:26 +0200

    Changed decoded.txt to user input

--- Something.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-user_input = input("Drag file here ")
-directory = os.path.dirname(user_input)
-new_file_path = os.path.join(directory, "decoded.txt")
-print(new_file_path)
-
-import time
-time.sleep(5)
